e7d7e63e1397159a8a53f859574ed3b45be732c4
Oxozon <oxozon.o4@gmail.com>
2023-01-10 23:58:24 +0100

    add location checking

---
+++ app/src/main/java/com/oxozon/weatherapp/models/Constants.kt
@@ -0,0 +1,5 @@
+package com.oxozon.weatherapp.models
+
+class Constants {
+
+}
--- app/src/main/java/com/oxozon/weatherapp/views/MainActivity.kt
+++ app/src/main/java/com/oxozon/weatherapp/views/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.oxozon.weatherapp.views
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.location.Location
 import android.location.LocationManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
+import android.webkit.PermissionRequest
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.location.*
 import com.google.gson.Gson
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.oxozon.weatherapp.R
 import com.oxozon.weatherapp.models.WeatherModel
 import com.oxozon.weatherapp.services.RetrofitInstance
@@ -26,10 +40,85 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cityName: String
     private lateinit var sharedPref: SharedPreferences
 
+    private lateinit var mFusedLocationClient : FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         sharedPref = getPreferences(Context.MODE_PRIVATE)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        if (!isLocationEnabled()) {
+            Toast.makeText(this, "By zagwarantować aktualność danych wymagane jest połączenie internetowe", Toast.LENGTH_SHORT).show()
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity((intent))
+        } else {
+            Dexter.withActivity(this).withPermissions(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()) {
+                            requestLocationData()
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied) {
+                            Toast.makeText(this@MainActivity, "By zagwarantować aktualność danych wymagane jest połączenie internetowe", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread().check()
+            Toast.makeText(this, "Aplikacja pobrała najnowsze dane bazując na Twojej obecnej lokalizacji", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            val latitude = mLastLocation.latitude
+            Log.i("Current latitude", "$latitude")
+
+            val longitude = mLastLocation.longitude
+            Log.i("Current Longitude", "$longitude")
+        }
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("Wygląda na to, że wyłączyłeś rekomendowane usługi internetowe!")
+            .setPositiveButton("PRZEJDŹ DO USTAWIEŃ") {
+                _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("ODRZUĆ") {
+                dialog, _ -> dialog.dismiss()
+            }.show()
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
     private fun getCurrentWeatherData(city: String) {
